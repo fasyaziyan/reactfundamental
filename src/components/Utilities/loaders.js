13e40ea413e0fe1ab0d9941f9f97d235fb300ee9
Fasyaziyan <fasyaziyan@gmail.com>
2022-08-21 15:01:38 +0700

    First Commit

---
+++ src/components/Utilities/loaders.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
+
+const Loaders = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+            <div className="d-flex">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+            </div>
+        </div>
+    )
+}
+
+export default Loaders
